ddc8a2ce4abf71f1ef9e06a6b1143fd93a7921fe
Jasmine Angle <jasmine@angle.dev>
2026-09-26 09:37:39 -0700

    Fix tooltips

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,18 +1,22 @@
 const email = "VUVkRloyRklTbXhhYWpCcFlsZEdjR0pJVW5aUGJYQm9Zekl4Y0dKdFZrRlpWelZ1WWtkVmRWcEhWakpKYWpWeFdWaE9kR0ZYTld4UlIwWjFXako0YkV4dFVteGthbmQyV1ZRMFBRPT0=";
-document.getElementById('show-email').addEventListener('click',function(event) {
-  event.preventDefault();
-  document.getElementById('email-contents').innerHTML = atob(atob(atob(email)));
-});
+// try {
+// document.getElementById('show-email').addEventListener('click',function(event) {
+//   event.preventDefault();
+//   document.getElementById('email-contents').innerHTML = atob(atob(atob(email)));
+// });
+// } catch {
+
+// }
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
-document.getElementById('toggle-jas-font').addEventListener('click',function(event) {
-  event.preventDefault();
-  var result = document.documentElement.classList.toggle('no-jas-font');
-  localStorage.setItem("no-jas-font", result?"1":"0");
-})
+// document.getElementById('toggle-jas-font').addEventListener('click',function(event) {
+//   event.preventDefault();
+//   var result = document.documentElement.classList.toggle('no-jas-font');
+//   localStorage.setItem("no-jas-font", result?"1":"0");
+// })
 function genTooltip(el) {
   var tltp = document.createElement('span');
   tltp.className = 'tltp';
@@ -28,11 +32,18 @@ function mkTooltip(e) {
   var tooltip = anchor.classList.contains("tltp")
     ? anchor : anchor.querySelector(":scope .tltp");
   if (!tooltip) tooltip = genTooltip(anchor);
+
+  // update: when parent display=relative and it has transform applied, fixed becomes absolute.
+  const parentContainer = tooltip.offsetParent || document.body;
+  const containerRect = parentContainer.getBoundingClientRect();
+  const fixedX = e.clientX - containerRect.left;
+  const fixedY = e.clientY - containerRect.top;
+
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
   tooltip.style.left = (e.clientX + tooltip.clientWidth + 15 < vw)
-    ? (e.clientX + 10 + "px") : (e.clientX - tooltip.clientWidth + "px");
+    ? (fixedX + 10 + "px") : (fixedX - tooltip.clientWidth + "px");
   tooltip.style.top = (e.clientY - tooltip.clientHeight - 5 > 0)
-    ? (e.clientY - tooltip.clientHeight + "px") : (e.clientY + 10 + "px");
+    ? (fixedY - tooltip.clientHeight + "px") : (fixedY + 10 + "px");
 }
 var links = document.querySelectorAll('a');
 for(var i = 0; i < links.length; i++) {
